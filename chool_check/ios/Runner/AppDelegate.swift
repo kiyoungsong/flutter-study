bf55f5de76dfef5f6bd77c9bad135827dac67158
ky.song <thdrldud369@gmail.com>
2023-12-25 14:16:22 +0900

    🔧 [Chore]: 프로젝트 설정 및 초기화

--- chool_check/ios/Runner/AppDelegate.swift
+++ chool_check/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("발근된 API 키 입력")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
