312fab5b691f192d619851fdfb98ca9255477fcc
ky.song <thdrldud369@gmail.com>
2024-01-09 15:37:57 +0900

    🚩 [Feat]: DB 연동

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import "package:get_it/get_it.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // intl 패키지 초기화(다국어화)
   await initializeDateFormatting("ko_kr", null);
+
+  // DB 생성
+  final database = LocalDatabase();
+  // GetIt에 데이터베이스 변수 주입
+  GetIt.I.registerSingleton<LocalDatabase>(database);
   runApp(MaterialApp(home: HomeScreen()));
 }
