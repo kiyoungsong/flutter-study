9c93f72de52dbb2b64bfc0dd213a7224b4d1394f
ky.song <thdrldud369@gmail.com>
2024-01-09 16:59:49 +0900

    🚩 [Feat]: 프로바이더 초기화

--- calendar_scheduler_server/lib/main.dart
+++ calendar_scheduler_server/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
+import 'package:calendar_scheduler/repository/schedule_repository.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import "package:get_it/get_it.dart";
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +14,16 @@ void main() async {
 
   // DB 생성
   final database = LocalDatabase();
+
+  final repository = ScheduleRepository();
+  final scheduleProvider = ScheduleProvider(repository: repository);
+
   // GetIt에 데이터베이스 변수 주입
   GetIt.I.registerSingleton<LocalDatabase>(database);
-  runApp(MaterialApp(home: HomeScreen()));
+
+  // Provider 하위 위젯에 제공하기
+  runApp(ChangeNotifierProvider(
+    create: (_) => scheduleProvider,
+    child: MaterialApp(home: HomeScreen()),
+  ));
 }
