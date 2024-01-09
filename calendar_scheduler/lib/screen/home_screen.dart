671cdd2876dfc2ad24a2ea2391549fae53580c3e
ky.song <thdrldud369@gmail.com>
2024-01-09 15:37:45 +0900

    🚩 [Feat]: 드리프트 함수 값 읽어오기 추가

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -3,7 +3,9 @@ import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/const/colors.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,7 +28,9 @@ class _HomeScreenState extends State<HomeScreen> {
           showModalBottomSheet(
               context: context,
               isDismissible: true, // 배경 탭 했을때 화면닫기
-              builder: (_) => const ScheduleBottomSheet(),
+              builder: (_) => ScheduleBottomSheet(
+                    selectedDate: selectedDate,
+                  ),
               // BottomSheet의 높이를 화면의 최대 높이로 정의하고 스크롤 가능하게 변경
               isScrollControlled: true);
         },
@@ -42,11 +46,54 @@ class _HomeScreenState extends State<HomeScreen> {
           const SizedBox(
             height: 8.0,
           ),
-          TodayBanner(selectedDate: selectedDate, count: 0),
+          StreamBuilder<List<Schedule>>(
+            stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+            builder: (context, snapshot) {
+              return TodayBanner(
+                  selectedDate: selectedDate,
+                  count: snapshot.data?.length ?? 0);
+            },
+          ),
           const SizedBox(
             height: 8.0,
           ),
-          const ScheduleCard(startTime: 12, endTime: 14, content: "프로그래밍 공부")
+          Expanded(
+              // 남은 공간 모두 차지
+              child: StreamBuilder<List<Schedule>>(
+            // 일정 정보가 Stream으로 제공 되기 때문
+            stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+            builder: (context, snapshot) {
+              // 데이터가 없을 대
+              if (!snapshot.hasData) {
+                return Container();
+              }
+
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  // 현재 index에 해당되는 일정
+                  final schedule = snapshot.data![index];
+
+                  return Dismissible(
+                      key: ObjectKey(schedule.id),
+                      direction: DismissDirection.startToEnd,
+                      onDismissed: (DismissDirection direction) {
+                        // 밀기 했을때 실핼 함수
+                        GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 8.0, left: 8.0, right: 8.0),
+                        child: ScheduleCard(
+                          startTime: schedule.startTime,
+                          endTime: schedule.endTime,
+                          content: schedule.content,
+                        ),
+                      ));
+                },
+              );
+            },
+          ))
         ],
       )),
     );
